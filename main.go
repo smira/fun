c70679115ba8da7d7e5825b4783089a01a5382a4
Andrey Smirnov <andrey.smirnov@talos-systems.com>
2023-05-28 13:55:24 +0400

    feat: implement 'main' for twosum
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/smira/fun/queens"
+	"github.com/smira/fun/twosum"
 )
 
 // Run me with:
@@ -25,5 +30,48 @@ func main() {
 		for _, solution := range solutions {
 			queens.PrintSolution(solution)
 		}
+	case "twosum":
+		fmt.Print("enter numbers: ")
+
+		reader := bufio.NewReader(os.Stdin)
+
+		s, _, err := reader.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var input twosum.Input
+
+		for _, elem := range strings.Split(string(s), " ") {
+			elem = strings.TrimSpace(elem)
+			if elem == "" {
+				continue
+			}
+
+			n, err := strconv.Atoi(elem)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			input = append(input, n)
+		}
+
+		fmt.Print("enter target: ")
+
+		var target int
+
+		if _, err := fmt.Scanf("%d\n", &target); err != nil {
+			log.Fatal(err)
+		}
+
+		a, b := twosum.BruteForceSolver(input, target)
+
+		if a == 0 && b == 0 {
+			fmt.Print("no solution found\n")
+		} else {
+			fmt.Printf("%d + %d = %d\n", a, b, target)
+		}
+	default:
+		log.Fatal("usage: fun queens|twosum")
 	}
 }
